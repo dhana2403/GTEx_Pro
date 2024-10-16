593f81fa80c58c1b492187794af0f70ca03a2b76
Dhanalakshmi Jothi <91247537+dhana2403@users.noreply.github.com>
2024-10-16 13:13:31 +0200

    Update 05_04_data_visualization_heatmapclustering.R

--- 5_Data_visualization/scripts/05_04_data_visualization_heatmapclustering.R
+++ 5_Data_visualization/scripts/05_04_data_visualization_heatmapclustering.R
@@ -1,9 +1,8 @@
 
 ################################################ VISUALIZATION BY HEATMAPCLUSTERING #####################################
 
-# Load necessary libraries
 library(ComplexHeatmap)
-library(circlize)  # For colorRamp2 function
+library(circlize) 
 
 # Define the path to the folder containing normalized count values
 data_path <- "./data/processed/expression/adjusted_sva"
@@ -20,10 +19,8 @@ gene_names <- #INPUT YOUR GENEID OF INTEREST
 # Loop through each tissue file
 for (tissue_file in tissue_files) {
   
-  # Extract the tissue name
   tissue_name <- gsub(".rds$", "", basename(tissue_file))
   
-  # Load the normalized read counts data from the .rds file
   normalized_counts <- readRDS(tissue_file)
   
   # Ensure the number of gene names matches the number of rows
@@ -34,7 +31,6 @@ for (tissue_file in tissue_files) {
   # Replace rownames with the manually defined gene names
   rownames(normalized_counts) <- gene_names
   
-  # Convert to data frame and log-transform
   normalized_counts <- as.data.frame(normalized_counts)
   data_log10 <- log10(normalized_counts + 1e-9)  # Add a small constant to avoid log(0)
   
@@ -47,7 +43,6 @@ for (tissue_file in tissue_files) {
   
   data_zscore_reordered <- as.matrix(data_zscore)
 
-  # Create a directory to save the plots for the current tissue
   result_directory <- file.path("results_after_sva_heatmap_cluster", tissue_name)
   dir.create(result_directory, recursive = TRUE, showWarnings = FALSE)
   
@@ -62,7 +57,6 @@ for (tissue_file in tissue_files) {
     color = colorRampPalette(c("blue", "white", "red"))(50)  # Color gradient
   )
   
-  # Save the heatmap as PDF and PNG with larger dimensions
   pdf(file.path(result_directory, paste0('heatmap_cluster_', tissue_name, '_vertical.pdf')), width = 20, height = 16)
   draw(p)  # Use draw() to render the heatmap
   dev.off()
@@ -72,5 +66,4 @@ for (tissue_file in tissue_files) {
   dev.off()
 }
 
-# Clean up
 rm(list = ls())
