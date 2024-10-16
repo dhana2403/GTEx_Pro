43190aaa67101581ff9d575570c601af796682f9
Dhanalakshmi Jothi <91247537+dhana2403@users.noreply.github.com>
2024-10-16 16:32:03 +0200

    Update 05_03_data_visualization_UMAP.R

--- 5_Data_visualization/scripts/05_03_data_visualization_UMAP.R
+++ 5_Data_visualization/scripts/05_03_data_visualization_UMAP.R
@@ -5,7 +5,6 @@ library(ggplot2)
 library(edgeR)
 library(umap)  
 
-# Define the path to the folder containing normalized count values
 data_path <- "./data/processed/expression/adjusted_sva"
 
 # List all .rds files under the data path
