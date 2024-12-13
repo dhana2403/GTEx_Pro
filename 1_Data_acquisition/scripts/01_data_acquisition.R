c671729bd139f570e69e741eee0680c9ba6ee0d5
Dhanalakshmi Jothi <91247537+dhana2403@users.noreply.github.com>
2024-12-13 13:50:56 +0100

    Update 01_data_acquisition.R

--- 1_Data_acquisition/scripts/01_data_acquisition.R
+++ 1_Data_acquisition/scripts/01_data_acquisition.R
@@ -1,10 +1,17 @@
 
 ################################Data acquisition#################################################
 
-system('mkdir -p data/raw')
+ystem('mkdir -p data/raw')
 system('mkdir -p data/processed')
 system('mkdir -p data/metadata')
-system('mkdir -p scripts_gtex')
 
-#download the metadata and gene expression files from https://gtexportal.org/home/downloads/adult-gtex/bulk_tissue_expression and move the metadata to data/metadata/ and gene expression data to data/raw
+library(tidyverse)
+
+system('wget -nv https://storage.googleapis.com/adult-gtex/annotations/v8/metadata-files/GTEx_Analysis_v8_Annotations_SampleAttributesDD.xlsx -P data/metadata')
+system('wget -nv https://storage.googleapis.com/adult-gtex/annotations/v8/metadata-files/GTEx_Analysis_v8_Annotations_SubjectPhenotypesDD.xlsx -P data/metadata')
+system('wget -nv https://storage.googleapis.com/adult-gtex/annotations/v8/metadata-files/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt -P data/metadata')
+system('wget -nv https://storage.googleapis.com/adult-gtex/annotations/v8/metadata-files/GTEx_Analysis_v8_Annotations_SubjectPhenotypesDS.txt -P data/metadata')
+system('wget -nv https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz -P data/raw')
+system('cd data/raw; gunzip *')
+
 
